5f6e8460ba72fea2ad0e430fc3246321f1f153fa
Aldana Albani <albani.aldana@gmail.com>
2025-05-04 22:14:45 -0300

    First commit

---
+++ src/api/materia-movement/routes/materia-movement.ts
@@ -0,0 +1,7 @@
+/**
+ * materia-movement router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::materia-movement.materia-movement');
---
+++ src/api/materia-movement/services/materia-movement.ts
@@ -0,0 +1,7 @@
+/**
+ * materia-movement service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::materia-movement.materia-movement');
